5fa11c4d9bb2465d0d6e74abe16324995546776b
kabirvirji <kabirvirji@gmail.com>
2019-10-03 18:35:50 -0400

    updated

--- server.js
+++ server.js
@@ -5,17 +5,31 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+const dotenv = require('dotenv');
+dotenv.config();
+const accountSid = process.env.SID;
+const authToken = process.env.TOKEN;
+const client = require('twilio')(accountSid, authToken);
 
 app.post('/', (req, res) => {
 
-  const twiml = new MessagingResponse();
+  // const twiml = new MessagingResponse();
 
   if (req.body.Body !== undefined) {
-    twiml.message(req.body.Body);
+    // twiml.message(req.body.Body);
+    // const client = require('twilio')(accountSid, authToken);
+
+    client.messages
+      .create({
+         body: req.body.Body,
+         from: '+12267969273',
+         to: '+16046448096'
+       })
+      .then(message => console.log(message.sid));
   }
   
   res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
+  // res.end(twiml.toString());
 });
 
 http.createServer(app).listen(1337, () => {
